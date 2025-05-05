d764a141a4547fbfd7cd7139e3a201fbc258abb1
alexpp9 <alexandrup.pirtac@outlook.com>
2025-05-05 08:13:22 +0300

    Added isAdmin attribute to user

--- models/user.js
+++ models/user.js
@@ -21,6 +21,7 @@ const UserSchema = new Schema(
       required: true,
       minLength: [8, 'Password must be at least 8 characters long.'],
     },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
