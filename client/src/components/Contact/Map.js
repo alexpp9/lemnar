e73bd5d70dab4cb9bc42751d601aa84b36af9789
alexpp9 <alexandrup.pirtac@outlook.com>
2025-08-07 17:22:25 +0100

    Added map with pin location

--- client/src/components/Contact/Map.js
+++ client/src/components/Contact/Map.js
@@ -1,22 +1,38 @@
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+const centerPosition = [46.9912496612831, 26.949505559324678];
+const lemnarPosition = [46.996592394744276, 26.938211518481594];
+const googleMapsUrl = 'https://maps.app.goo.gl/XHZQnKhMf8GhgWYq5';
 const Map = () => {
+  // Custome Icon Marker
+  const customIcon = new Icon({
+    iconUrl:
+      'https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/location-24-512.png',
+    iconSize: [25, 25],
+  });
+
   return (
-    <>
-      <div className="col-12 col-md-6">
-        <div className="card shadow-sm">
-          <div className="card-body p-0">
-            <iframe
-              title="Company Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!..." // <-- Replace with your actual location embed
-              width="100%"
-              height="450"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-            ></iframe>
-          </div>
-        </div>
-      </div>
-    </>
+    <MapContainer
+      className="col-12 col-md-6 shadow-sm  border rounded"
+      center={centerPosition}
+      zoom={10}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={lemnarPosition} icon={customIcon}>
+        <Popup>
+          <h5>
+            Get direction to{' '}
+            <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer">
+              Lemnar
+            </a>
+          </h5>
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
