18d2a982bfb01a571875430e943f0112c14e6d66
alexpp9 <alexandrup.pirtac@outlook.com>
2025-08-16 14:45:58 +0100

    Email configuration OK

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,21 +1,23 @@
-const nodemailer = require('nodemailer');
+// .env variables at the top
 const { email_host, email_password } = require('../config');
-
+const nodemailer = require('nodemailer');
 module.exports.sendEmail = (req, res) => {
   const { name, email, subject, message } = req.body;
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: email_host,
-      password: email_password,
+      pass: email_password,
     },
   });
-
+  // I used ChatGPT here to understand how the service works
+  // Why I added replyTo as a way to get the senders email address.
   const mailOptions = {
-    from: email,
+    from: email_host,
     to: email_host,
     subject: `Message from ${name}: ${subject}`,
     text: message,
+    replyTo: email,
   };
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
