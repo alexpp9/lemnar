32b3110982d9dff89d344ef6b2bc9370a1e8b515
alexpp9 <alexandrup.pirtac@outlook.com>
2025-09-06 19:23:14 +0100

    Solved the DB connection to Atlas

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 // process.env.MONGO_URL
 module.exports = {
   port: process.env.PORT || 3000,
-  // mongoURL: 'mongodb://localhost:27017/lemnar-testDB',
   mongoURL: 'mongodb://localhost:27017/lemnar',
   sessionSecret: process.env.SECRET_SESSION,
   email_password: process.env.EMAIL_PASSWORD,
   email_host: process.env.EMAIL_HOST,
+  dbUrl: process.env.DB_URL,
 };
--- db.js
+++ db.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
-const { mongoURL } = require('./config');
+const { mongoURL, dbUrl } = require('./config');
 
 // Connection to the DB (clurster)
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      mongoURL
-      // useNewUrlParser: true, Depricated message
-      // useUnifiedTopology: true, Depricated message
-    );
+    await mongoose.connect(dbUrl, {
+      family: 4,
+    });
     console.log(`Connection to DB established!`);
     console.log(`=============================`);
   } catch (error) {
     console.log(`Problem occured while tryin to connect to the DB!`);
     console.log(`=================================================`);
     console.error(error.message);
+    console.error(error);
     process.exit(1);
   }
 };
